46e6f3e83cd25e25f2629341c090ef04e331cdbe
Adam Leadbetter <adamml@users.noreply.github.com>
2021-12-07 09:23:46 +0000

    Day 7

---
+++ 2021/Day 7/day-7.py
@@ -0,0 +1,29 @@
+test_data = [16,1,2,0,4,2,7,1,2,14]
+
+with open('input.txt', 'r') as f:
+    data = [int(x) for x in f.readlines()[0].split(',')]
+
+ipt = data
+
+cost = []
+
+for x in range(min(ipt), max(ipt)+1):
+    this_cost = 0
+    for xx in ipt:
+        this_cost += abs(x - xx)
+    cost.append(this_cost)
+
+print(min(cost))
+
+cost = []
+
+costs = range(min(ipt), max(ipt)+1)
+print(costs)
+
+for x in range(min(ipt), max(ipt)+1):
+    this_cost = 0
+    for xx in ipt:
+        this_cost += sum(costs[0:abs(x - xx)+1])
+    cost.append(this_cost)
+
+print(min(cost))
