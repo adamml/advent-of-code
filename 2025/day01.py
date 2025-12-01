ee5dcae931647f652d666a961c4ab9beac633df7
Adam Leadbetter <adamml@users.noreply.github.com>
2025-12-01 16:11:37 +0000

    Add 2025 Day 1

---
+++ 2025/day01.py
@@ -0,0 +1,54 @@
+from math import floor
+
+test = ["L68",
+        "L30",
+        "R48",
+        "L5",
+        "R60",
+        "L55",
+        "L1",
+        "L99",
+        "R14",
+        "L82"]
+
+doTest = False
+method = 2  # Method 1 = Step 1 method; Method 2 = Step 2 method
+
+def processStep(point, password, method, step):
+    thisPoint = int(step[1:]) % 100
+    if method == 2:
+        password = password + floor(int(step[1:]) / 100)
+    if step[0] == 'L':
+        if point == 0:
+            password -= 1
+        point = point - thisPoint
+    else:
+        point = point + thisPoint
+    if point > 99:
+        point = point - 100
+        if method == 2:
+            if point != 0:
+                password += 1
+    elif point < 0:
+        point = 100 + point
+        if method == 2:
+            password += 1
+    if point == 0:
+        password += 1
+    return [point, password]
+
+
+point = 50
+password = 0
+
+
+if doTest != True:
+    with open('./input/day01.txt') as f:
+        for x in f:
+            [point, password] = processStep(point, password, method, x)
+        f.close()
+else:
+    for x in test:
+        [point, password] = processStep(point, password, method, x)
+
+print(password)
